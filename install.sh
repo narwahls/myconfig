0e94b07e4b798c3843fffc3fc227268947f19846
Sven Behrend <bse666@gmail.com>
2013-09-21 14:10:29 +0200

    zsh config hinzugefügt.

--- install.sh
+++ install.sh
@@ -10,8 +10,9 @@ rm $HOME/.Xresources; ln -s $CURRENTDIR/.Xresources $HOME/.Xresources
 rm $HOME/.tmux.conf; ln -s $CURRENTDIR/.tmux.conf $HOME/.tmux.conf
 rm $HOME/.vimrc; ln -s $CURRENTDIR/.vimrc $HOME/.vimrc
 rm -rf $HOME/.config/awesome; ln -s $CURRENTDIR/awesome $HOME/.config/awesome
-rm $HOME/.ncmpcpp; ln -s $CURRENTDIR/ncmpcpp $HOME/.ncmpcpp
+rm -rf $HOME/.ncmpcpp; ln -s $CURRENTDIR/ncmpcpp $HOME/.ncmpcpp
 rm -rf $HOME/.i3; ln -s $CURRENTDIR/i3 $HOME/.i3
 
 rm -rf $HOME/.vim; ln -s $CURRENTDIR/vim $HOME/.vim
+rm -rf $HOME/.zshrc; ln -s $CURRENTDIR/.zshrc $HOME/.zshrc
 
