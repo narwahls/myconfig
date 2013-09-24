0e70afbc0acd8c8118a7d46afbec7a9b74fbc253
Sven Behrend <bse666@gmail.com>
2013-09-24 20:55:29 +0200

    zsh installation hinzugefügt

--- install.sh
+++ install.sh
@@ -16,6 +16,13 @@ rm -rf $HOME/.ncmpcpp; ln -s $CURRENTDIR/ncmpcpp $HOME/.ncmpcpp
 rm -rf $HOME/.i3; ln -s $CURRENTDIR/i3 $HOME/.i3
 
 rm -rf $HOME/.vim; ln -s $CURRENTDIR/vim $HOME/.vim
-rm -rf $HOME/.zshrc; ln -s $CURRENTDIR/.zshrc $HOME/.zshrc
+# old/standard zsh config
+#rm -rf $HOME/.zshrc; ln -s $CURRENTDIR/.zshrc $HOME/.zshrc
 
 sudo sed -i_bak 's#NoDisplay=true#NoDisplay=false#g' awesome.desktop
+
+rm -rf $HOME/.zshrc; cp $CURRENTDIR/.eh-my-zsh/templastes/zshrc.zsh-template $HOME/.zshrc
+sed -i "s#ZSH=\$HOME/.oh-my-zsh#ZSH=$CURRENTDIR/oh-my-zsh#g" $HOME/.zshrc
+# set default shell
+chsh -s /bin/zsh
+
